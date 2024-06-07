687ce80bd1830b17f058c8bd4b2c54a0b2ead29d
PhoenixGS <thestarrydream@gmail.com>
2024-06-08 00:17:13 +0800

    update spmm_opt.cu 一个线程计算B矩阵中多个列

--- PA3/src/spmm_opt.cu
+++ PA3/src/spmm_opt.cu
@@ -1,12 +1,18 @@
 #include "spmm_opt.h"
 
+const int C = 2;
+
 __global__ void spmm_kernel_placeholder(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v, int INFEATURE)
 {
     __shared__ int shared_idx[32];
     __shared__ float shared_val[32];
-    float ans = 0.0;
+    float ans[C];
+    for (int i = 0; i < C; i++)
+    {
+        ans[i] = 0.0;
+    }
     int indx = blockIdx.x;
-    int indy = blockIdx.y * 32 + threadIdx.x;
+    int indy = blockIdx.y * 32 * C + threadIdx.x;
     int begin = ptr[indx], end = ptr[indx + 1];
     for (int now = begin; now < end; now += 32)
     {
@@ -18,16 +24,22 @@ __global__ void spmm_kernel_placeholder(int *ptr, int *idx, float *val, float *v
         __syncthreads();
         for (int i = 0; i < 32 && now + i < end; i++)
         {
-            ans += shared_val[i] * vin[shared_idx[i] * INFEATURE + indy];
+            for (int j = 0; j < C; j++)
+            {
+                ans[j] += shared_val[i] * vin[shared_idx[i] * INFEATURE + indy + j * 32];
+            }
         }
     }
-    vout[indx * INFEATURE + indy] = ans;
+    for (int j = 0; j < C; j++)
+    {
+        vout[indx * INFEATURE + indy + j * 32] = ans[j];
+    }
 }
 void SpMMOpt::preprocess(float *vin, float *vout)
 {
     int WARP_SIZE = 32;
     grid.x = num_v;
-    grid.y = (feat_in + WARP_SIZE - 1) / WARP_SIZE;
+    grid.y = (feat_in + WARP_SIZE * C - 1) / WARP_SIZE / C;
     block.x = WARP_SIZE;
 }
 
