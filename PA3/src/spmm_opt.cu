3169979771d5d73806bc046ca78a9071161c1fd3
PhoenixGS <thestarrydream@gmail.com>
2024-06-09 16:38:28 +0800

    update spmm_opt in PA3

--- PA3/src/spmm_opt.cu
+++ PA3/src/spmm_opt.cu
@@ -1,20 +1,47 @@
 #include "spmm_opt.h"
 
-const int C = 1;
-const int D = 1;
+// const int C = 4;
+const int D = 2;
 const int SPLIT_SIZE = 128;
 
-__global__ void spmm_kernel_placeholder(int *ptr, int *idx, float *val, float *vin, float *vout, int *target, int *ptr_scheduled, int num_v, int INFEATURE, bool dataset)
+__global__ void spmm_kernel_placeholder32(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v, int INFEATURE)
 {
     __shared__ int shared_idx[32][D];
     __shared__ float shared_val[32][D];
-    float ans[C];
-    for (int i = 0; i < C; i++)
-    {
-        ans[i] = 0.0;
-    }
+    float ans = 0.0;
     int indx = blockIdx.x;
-    int indy = blockIdx.y * 32 * C + threadIdx.x;
+    int indy = blockIdx.y * 32 + threadIdx.x;
+	// int begin = ptr[indx], end = ptr[indx + 1];
+	int begin = ptr[indx], end = ptr[indx + 1];
+	for (int now = begin; now < end; now += 32 * D)
+    {
+		for (int k = 0; k < D; k++)
+		{
+			if (now + threadIdx.x + k * 32 < end)
+			{
+				shared_idx[threadIdx.x][k] = idx[now + threadIdx.x + k * 32];
+				shared_val[threadIdx.x][k] = val[now + threadIdx.x + k * 32];
+			}
+		}
+        __syncthreads();
+		for (int k = 0; k < D; k++)
+		{
+			for (int i = 0; i < 32 && now + i + k * 32 < end; i++)
+			{
+				ans += shared_val[i][k] * vin[shared_idx[i][k] * INFEATURE + indy];
+			}
+		}
+    }
+	vout[indx * INFEATURE + indy] = ans;
+}
+
+__global__ void spmm_kernel_placeholder32p(int *ptr, int *idx, float *val, float *vin, float *vout, int *target, int *ptr_scheduled, int num_v, int INFEATURE)
+{
+    __shared__ int shared_idx[32][D];
+    __shared__ float shared_val[32][D];
+    float ans = 0.0;
+    int indx = blockIdx.x;
+    int indy = blockIdx.y * 32 + threadIdx.x;
 	// int begin = ptr[indx], end = ptr[indx + 1];
 	int begin = ptr_scheduled[indx], end = ptr_scheduled[indx + 1];
 	for (int now = begin; now < end; now += 32 * D)
@@ -32,14 +59,89 @@ __global__ void spmm_kernel_placeholder(int *ptr, int *idx, float *val, float *v
 		{
 			for (int i = 0; i < 32 && now + i + k * 32 < end; i++)
 			{
-				for (int j = 0; j < C; j++)
+				ans += shared_val[i][k] * vin[shared_idx[i][k] * INFEATURE + indy];
+			}
+		}
+    }
+	atomicAdd(vout + target[indx] * INFEATURE + indy, ans);
+}
+
+__global__ void spmm_kernel_placeholder2(int *ptr, int *idx, float *val, float *vin, float *vout, int *target, int *ptr_scheduled, int num_v, int INFEATURE, bool dataset)
+{
+    __shared__ int shared_idx[32][D];
+    __shared__ float shared_val[32][D];
+    float ans[2] = {0.0, 0.0};
+    int indx = blockIdx.x;
+    int indy = blockIdx.y * 32 * 2 + threadIdx.x;
+	// int begin = ptr[indx], end = ptr[indx + 1];
+	int begin = ptr_scheduled[indx], end = ptr_scheduled[indx + 1];
+	for (int now = begin; now < end; now += 32 * D)
+    {
+		for (int k = 0; k < D; k++)
+		{
+			if (now + threadIdx.x + k * 32 < end)
+			{
+				shared_idx[threadIdx.x][k] = idx[now + threadIdx.x + k * 32];
+				shared_val[threadIdx.x][k] = val[now + threadIdx.x + k * 32];
+			}
+		}
+        __syncthreads();
+		for (int k = 0; k < D; k++)
+		{
+			for (int i = 0; i < 32 && now + i + k * 32 < end; i++)
+			{
+				for (int j = 0; j < 2; j++)
 				{
 					ans[j] += shared_val[i][k] * vin[shared_idx[i][k] * INFEATURE + indy + j * 32];
 				}
 			}
 		}
     }
-    for (int j = 0; j < C; j++)
+    for (int j = 0; j < 2; j++)
+    {
+		if (dataset)
+		{
+			atomicAdd(vout + target[indx] * INFEATURE + indy + j * 32, ans[j]);
+		}
+		else
+		{
+			vout[indx * INFEATURE + indy + j * 32] = ans[j];
+		}
+    }
+}
+
+__global__ void spmm_kernel_placeholder4(int *ptr, int *idx, float *val, float *vin, float *vout, int *target, int *ptr_scheduled, int num_v, int INFEATURE, bool dataset)
+{
+    __shared__ int shared_idx[32][D];
+    __shared__ float shared_val[32][D];
+    float ans[4] = {0.0, 0.0, 0.0, 0.0};
+    int indx = blockIdx.x;
+    int indy = blockIdx.y * 32 * 4 + threadIdx.x;
+	// int begin = ptr[indx], end = ptr[indx + 1];
+	int begin = ptr_scheduled[indx], end = ptr_scheduled[indx + 1];
+	for (int now = begin; now < end; now += 32 * D)
+    {
+		for (int k = 0; k < D; k++)
+		{
+			if (now + threadIdx.x + k * 32 < end)
+			{
+				shared_idx[threadIdx.x][k] = idx[now + threadIdx.x + k * 32];
+				shared_val[threadIdx.x][k] = val[now + threadIdx.x + k * 32];
+			}
+		}
+        __syncthreads();
+		for (int k = 0; k < D; k++)
+		{
+			for (int i = 0; i < 32 && now + i + k * 32 < end; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					ans[j] += shared_val[i][k] * vin[shared_idx[i][k] * INFEATURE + indy + j * 32];
+				}
+			}
+		}
+    }
+    for (int j = 0; j < 4; j++)
     {
 		if (dataset)
 		{
@@ -62,8 +164,14 @@ bool SpMMOpt::check_dataset()
 
 void SpMMOpt::preprocess(float *vin, float *vout)
 {
+	int C = 1;
+	if (feat_in == 256)
+	{
+		C = 2;
+		if (num_v == 1138499 || num_v == 2500604 || num_v == 881680) C = 4;
+	}
 	bool dataset = check_dataset();
-	fprintf(stderr, "dataset = %d", dataset);
+	fprintf(stderr, "dataset = %d, C = %d\n", dataset, C);
 
 	if (! dataset)
 	{
@@ -120,5 +228,27 @@ void SpMMOpt::preprocess(float *vin, float *vout)
 
 void SpMMOpt::run(float *vin, float *vout)
 {
-	spmm_kernel_placeholder<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, target, ptr_scheduled, num_v, feat_in, check_dataset());
+	if (feat_in == 32)
+	{
+		if (check_dataset())
+		{
+			spmm_kernel_placeholder32p<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, target, ptr_scheduled, num_v, feat_in);
+		}
+		else
+		{
+			spmm_kernel_placeholder32<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, num_v, feat_in);
+		}
+	}
+	else
+	{
+		if (num_v == 1138499 || num_v == 2500604 || num_v == 881680)
+		{
+			spmm_kernel_placeholder4<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, target, ptr_scheduled, num_v, feat_in, check_dataset());
+		}
+		else
+		{
+			spmm_kernel_placeholder2<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, target, ptr_scheduled, num_v, feat_in, check_dataset());
+		}
+
+	}
 }
